acc20ac6d51dfebbca25596cbf80f806f8c71052
vigbalar <vigbalar@gmail.com>
2024-05-14 00:07:21 +0530

    updated import wtforms

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtform import StringField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
