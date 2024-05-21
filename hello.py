0af89d5ccc7fb53a7d1bc7879b118399d6ab3653
vigbalar <vigbalar@gmail.com>
2024-05-21 16:00:23 +0530

    updated files with flash messaging

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -25,9 +25,11 @@ def internal_server_error(e):
     
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not  None and old_name != form.name.data:
+            flash('Looks like the name has been changed!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
